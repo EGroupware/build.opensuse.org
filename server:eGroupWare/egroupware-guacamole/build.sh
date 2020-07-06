e92e58150399f8f2aa33865280c1e64cb2b7e887
Ralf Becker <RalfBecker@outdoor-training.de>
2020-07-06 20:58:14 +0200

    Guacamole 1.2.0.20200706

---
+++ server:eGroupWare/egroupware-guacamole/build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+VERSION=${1:-1.2.0.$(date +%Y%m%d)}
+PACKAGE=egroupware-guacamole
+GITURL=git@github.com:EGroupware/guacamole.git
+BUILD=/tmp/$PACKAGE-build-root
+OUTPUTDIR=$(dirname $0)
+[[ $OUTPUTDIR = /* ]] || OUTPUTDIR=$PWD/${OUTPUTDIR#./}
+TAROPTS="--exclude .git --exclude .gitignore --uid 0 --gid 0 -cvzf"
+
+
+rm -rf $BUILD $OUTPUTDIR/$PACKAGE-*.tar.gz
+mkdir -p $BUILD
+
+cd $BUILD
+
+git clone $GITURL $BUILD/$PACKAGE-$VERSION
+
+tar $TAROPTS $OUTPUTDIR/$PACKAGE-$VERSION.tar.gz $PACKAGE-$VERSION 
+
+sed -e "s/^Version:.*/Version: $VERSION/g" \
+    -e "s/$PACKAGE-.*/$PACKAGE-$VERSION/g" \
+    -i "" $OUTPUTDIR/$PACKAGE.dsc
+
+cd $OUTPUTDIR
+osc addremove
+osc status
