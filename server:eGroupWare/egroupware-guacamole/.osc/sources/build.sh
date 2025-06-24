2c4c22f783a5560f43b00fa2074ef980b986aa16
ralf <rb@egroupware.org>
2025-06-24 14:11:46 +0200

    fix not found doc/docker-compose.yml

--- server:eGroupWare/egroupware-guacamole/.osc/sources/build.sh
+++ server:eGroupWare/egroupware-guacamole/.osc/sources/build.sh
@@ -6,7 +6,7 @@ GITURL=https://github.com/EGroupware/guacamole.git
 BUILD=/tmp/$PACKAGE-build-root
 OUTPUTDIR=$(dirname $0)
 [[ $OUTPUTDIR = /* ]] || OUTPUTDIR=$PWD/${OUTPUTDIR#./}
-TAROPTS="--uid 0 --gid 0 -cvzf"
+TAROPTS="--uid 0 --gid 0 --no-xattrs --exclude .git -cvzf"
 
 
 rm -rf $BUILD $OUTPUTDIR/$PACKAGE-*.tar.gz
@@ -14,9 +14,9 @@ mkdir -p $BUILD
 
 cd $BUILD
 
-git clone $GITURL $BUILD/$PACKAGE-$VERSION
+git clone $GITURL $PACKAGE-$VERSION
 
-tar $TAROPTS $OUTPUTDIR/$PACKAGE-$VERSION.tar.gz $PACKAGE-$VERSION 
+(cd $PACKAGE-$VERSION; tar $TAROPTS $OUTPUTDIR/$PACKAGE-$VERSION.tar.gz .)
 
 sed -e "s/^Version:.*/Version: $VERSION/g" \
     -e "s/$PACKAGE-.*.tar.gz/$PACKAGE-$VERSION.tar.gz/g" \
